3f3804ffbbf0f25efc82e45d33526fd3f62a5fb0
Dorothy Brown <dkb@advertio.com>
2021-06-13 18:33:15 +0100

    add pictures

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Post(models.Model):
+    name = models.CharField(max_length=200)
+    content = models.TextField(max_length=1000)
+    image = models.ImageField(upload_to='blog/images/', blank=True)
+    date = models.DateField()
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from .models import Post
 # Create your views here.
+
+
+def all_blogs(request):
+    posts = list(Post.objects.order_by('-date')[:5])
+    length = len(posts)
+    return render(request, 'blog/all_blogs.html', {'posts': posts, 'length': length})
