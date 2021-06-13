b88808e958db23526775bc4374720fa2e94f2833
Dorothy Brown <dkb@advertio.com>
2021-06-13 19:03:42 +0100

    Add a detail page for blog posts

--- blog/views.py
+++ blog/views.py
@@ -7,3 +7,8 @@ def all_blogs(request):
     posts = list(Post.objects.order_by('-date')[:5])
     length = len(posts)
     return render(request, 'blog/all_blogs.html', {'posts': posts, 'length': length})
+
+
+def detail(request, post_id):
+    post = Post.objects.get(id=post_id)
+    return render(request, 'blog/detail.html', {'id': post_id, 'post': post})
