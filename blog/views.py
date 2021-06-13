560fce5db67fb2657431936470d36da5f066b943
Dorothy Brown <dkb@advertio.com>
2021-06-13 19:45:08 +0100

    Add static image to homepage

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 # Create your views here.
 
@@ -10,5 +10,5 @@ def all_blogs(request):
 
 
 def detail(request, post_id):
-    post = Post.objects.get(id=post_id)
+    post = get_object_or_404(Post, pk=post_id)
     return render(request, 'blog/detail.html', {'id': post_id, 'post': post})
--- personal_portfolio_django/settings.py
+++ personal_portfolio_django/settings.py
@@ -121,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_ROOT = str(BASE_DIR / 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = str(BASE_DIR / 'media')
